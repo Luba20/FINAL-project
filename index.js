1cee35f405a8357b207a9ce2e77a35c646368432
Luba20 <128219479+Luba20@users.noreply.github.com>
2024-05-12 11:06:59 +0200

    Added a function that gets the current date

--- index.js
+++ index.js
@@ -21,3 +21,25 @@ function changeTemp(response) {
   let icon = document.querySelector("#icon");
   icon.innerHTML = `<img src ="${response.data.condition.icon_url}" class = "weather-app-icon" />`;
 }
+function getDateTime(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
